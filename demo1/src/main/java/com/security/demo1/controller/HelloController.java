e74621f13ebdd1f9fef722eaba27b9b0cda78cd3
JionLin <250912986@qq.com>
2022-04-30 23:05:23 +0800

    改变

---
+++ demo1/src/main/java/com/security/demo1/controller/HelloController.java
@@ -0,0 +1,22 @@
+//package com.security.demo1.controller;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+///**
+// * @author johnny
+// * @Classname HelloController
+// * @Description
+// * @Date 2022/4/30 22:45
+// */
+//@RequestMapping("/test")
+//@RestController
+//public class HelloController {
+//
+//
+//    @GetMapping("hello")
+//    public String hello(){
+//        return "hello";
+//    }
+//}
